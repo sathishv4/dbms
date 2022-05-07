ed2f71eba0b7d4407d990ff6d1e44bf7464c001c
sathishv4 <104985374+sathishv4@users.noreply.github.com>
2022-05-07 23:28:11 +0530

    Add files via upload

---
+++ bindex.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DBMS Assignment</title>
+</head>
+<body>
+<center>
+<h3>Inserting into Boat Table</h3>
+<form action="binsert.php" method="post">
+<p>
+<label for="bid_">Boat ID:</label>
+<input type="text" name="bid" id="bid_">
+</p>
+<p>
+<label for="bname_">Boat Name:</label>
+<input type="text" name="bname" id="bname_">
+</p>
+<p>
+<label for="color_">Color:</label>
+<input type="text" name="color" id="color_">
+</p>
+<input type="submit" value="Submit">
+</form>
+</center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DBMS Assignment</title>
+</head>
+<body style="background-color:#fffdd0;">
+<style>
+#rcorners1 {
+  border-radius: 25px;
+  background: #FF7300;
+  padding: 20px; 
+  width: 300px;
+  height: 100px;  
+}
+#rcorners2 {
+  border-radius: 25px;
+  background: url(title.gif);
+  background-position: left top;
+  background-repeat: repeat;
+  padding: 20px; 
+  width: 300px;
+  height: 35px;  
+}
+#rcorners3 {
+  border-radius: 25px;
+  background: #00A300;
+  padding: 20px; 
+  width: 300px;
+  height: 20px;  
+}
+.button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1{background-color: #4CAF50;}
+.button2{background-color: #808080;}
+</style>
+
+<body>
+<center>
+<h3 id="rcorners2" style="border:2px solid Tomato;color:white;">Which table you wanna insert values in?</h3>
+<p id="rcorners1" style="color:darkblue">
+<a href="sindex.php">Sailors</a>
+<br></br>
+<a href="bindex.php">Boats</a>
+<br></br>
+<a href="rindex.php">Reserves</a>
+</p>
+<img src="chakra2.jpg"  width="60" height="60">
+
+<p id="rcorners3" style="color:darkblue" class="w3-medium">Powered by <a href="https://github.com/labhayl" target="_blank">Abhay</a></p>
+</center>
+</body>
+</html>
